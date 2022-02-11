686a5dce404b73db93134ef799745df14aefbe79
Shaurya Aditya Singh <59340919+Shaurya111001@users.noreply.github.com>
2022-02-11 18:46:30 +0530

    Update Screen.js

--- src/Screen.js
+++ src/Screen.js
@@ -19,38 +19,32 @@ const Screen = forwardRef(({ caption, scroll }, ref) => (
       </div>
       <div style={{ height: "200vh" }}>
         <div className="dot">
-          <h1>headphone</h1>
-          Headphones are a pair of small loudspeaker drivers worn on or around the head over a user's ears.
+          <h1>ALL GROUPS</h1>
         </div>
       </div>
       <div style={{ height: "200vh" }}>
         <div className="dot">
-          <h1>rocket</h1>A rocket (from Italian: rocchetto, lit. 'bobbin/spool')[nb 1][1] is a projectile that spacecraft, aircraft or other
-          vehicle use to obtain thrust from a rocket engine.
+          <h1>UpComing Events</h1>
         </div>
       </div>
       <div style={{ height: "200vh" }}>
         <div className="dot">
-          <h1>turbine</h1>A turbine (/ˈtɜːrbaɪn/ or /ˈtɜːrbɪn/) (from the Greek τύρβη, tyrbē, or Latin turbo, meaning vortex)[1][2] is a
-          rotary mechanical device that extracts energy from a fluid flow and converts it into useful work.
+          <h1>TeamLeads</h1>
         </div>
       </div>
       <div style={{ height: "200vh" }}>
         <div className="dot">
-          <h1>table</h1>A table is an item of furniture with a flat top and one or more legs, used as a surface for working at, eating from or
-          on which to place things.[1][2]
+          <h1>PartnerShips</h1>
         </div>
       </div>
       <div style={{ height: "200vh" }}>
         <div className="dot">
-          <h1>laptop</h1>A laptop, laptop computer, or notebook computer is a small, portable personal computer (PC) with a screen and
-          alphanumeric keyboard.
+          <h1>Footer</h1>
         </div>
       </div>
       <div style={{ height: "200vh" }}>
         <div className="dot">
-          <h1>zeppelin</h1>A Zeppelin is a type of rigid airship named after the German inventor Count Ferdinand von Zeppelin (German
-          pronunciation: [ˈt͡sɛpəliːn]) who pioneered rigid airship development at the beginning of the 20th century.
+          <h1>Others...</h1>
         </div>
       </div>
     </div>
